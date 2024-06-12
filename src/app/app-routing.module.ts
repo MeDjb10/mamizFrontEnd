920a3ca886c3264e2e787598c40c9906efd85aa3
MeDjb10 <131858818+MeDjb10@users.noreply.github.com>
2024-06-12 06:36:50 +0100

    Update routing to include AllEventsComponent and AllForumsComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { HomeComponent } from './components/static/home/home.component';
 import { AcceuilComponent } from './components/dynamic/home/acceuil/acceuil.component';
 import { AllAteliersComponent } from './components/dynamic/Atelier/all-ateliers/all-ateliers.component';
 import { AtelierDetailsComponent } from './components/dynamic/Atelier/atelier-details/atelier-details.component';
+import { AllEventsComponent } from './components/dynamic/Events/all-events/all-events.component';
+import { AllForumsComponent } from './components/dynamic/Forum/all-forums/all-forums.component';
 
 const routes: Routes = [
   {
@@ -14,6 +16,8 @@ const routes: Routes = [
       { path: '', redirectTo: 'acceuil', pathMatch: 'full' },
       { path: 'acceuil', title: ' Acceuil', component: AcceuilComponent },
       { path: 'atelier', title: ' Ateliers', component: AllAteliersComponent },
+      { path: 'event', title: ' Event', component: AllEventsComponent },
+      { path: 'forum', title: ' Forum', component: AllForumsComponent },
       { path: 'atelier-details/:id', component: AtelierDetailsComponent },
       { path: '**', redirectTo: 'acceuil' },
     ],
